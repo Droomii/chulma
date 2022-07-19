a645a06c9101c4b57a914fa473dc280cb508c53e
kdw0601 <kdw0601@midasin.com>
2022-07-19 17:52:35 +0900

    commmiitttttttt

--- App.tsx
+++ App.tsx
@@ -4,8 +4,13 @@ import { StyleSheet, Text, View } from 'react-native';
 export default function App() {
   return (
     <View style={styles.container}>
-      <Text>Open up App.tsx to start working on your app!</Text>
-      <StatusBar style="auto" />
+      {Array(7).fill(0).map((v, i1) => (
+          <View style={{flex: 1, flexDirection: 'row'}} key={i1}>
+            {Array(7).fill(0).map((v, i2) => (
+                <View key={i2} style={{flex: 1, backgroundColor: `rgb(${i1 * 14 + i2 * 2}, ${i1 * 14 + i2 * 2}, ${i1 * 14 + i2 * 2})`}}/>
+            ))}
+          </View>
+      ))}
     </View>
   );
 }
@@ -13,8 +18,7 @@ export default function App() {
 const styles = StyleSheet.create({
   container: {
     flex: 1,
-    backgroundColor: '#fff',
-    alignItems: 'center',
-    justifyContent: 'center',
+    backgroundColor: 'pink',
+    padding: 20,
   },
 });
---
+++ src/component/calendar/Calendar.tsx
@@ -0,0 +1,14 @@
+// Created by kdw0601 on 2022-07-19
+
+interface Props {
+
+}
+
+const Calendar = (props: Props) => {
+    return (
+        <>
+        </>
+    );
+};
+
+export default Calendar;
